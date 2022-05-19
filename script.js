a2d39496f15042fe29e691f3cda02043e0aad3c8
Sabrina <104360157+Sabzaroo@users.noreply.github.com>
2022-05-19 16:18:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 const config = {
-    title: "Inform your content with qualitative data",
-    question: "There are several tools you can use to understand which content your audience is actively engaging with.",
-    image_on: true,
+    title: "Click on each bullet below to learn more about each point.",
+    question: "",
+    image_on: false,
     image: "https://a.storyblok.com/f/112136/373x445/175f800354/lucia-8.png",
     selection_title: "Select the tool name below to see how:",
     selection_options: [
-        "Buzzsumo",
-        "Mentionlytics",
-        "Audiense",
-        "Google Trends"
+        "It will help us be more consistent",
+        "It will help us stand out from our competition",
+        "It will help us be clearer",
+        ""
     ],
     answer_options: [
-        "",
-        "Useful for monitoring your own content for measurement and benchmarking.",
-        "A great Twitter tool to monitor trends, build and segment lists of key users, monitor content performance and more.",
-        "Can show you how many people are talking about a topic. You can cross reference it with Reddit, Quora or a social media platform to validate the data and make a call on how impactful that content is."
+        "When our tone is consistent, partners trust us more since they know what to expect.",
+        "By simplifying and making our language more consistent, we'll stand out by being more relatable to our partners.",
+        "Our tone of voice will help us write more clearly so we can all save time reading.",
+        ""
     ],
     answer_background_colour: "rgba(37, 81, 123, 0.5)",
     answer_font_colour: "white",
     big_answer_image: [
-        "https://a.storyblok.com/f/112136/1001x354/be5144572e/aware.png",
-        "https://a.storyblok.com/f/112136/446x446/3ea9d4491c/2.png",
-        "https://a.storyblok.com/f/112136/446x446/1cadc0e6c3/3.png",
-        "https://a.storyblok.com/f/112136/446x446/0c7f465cc6/4.png",
+        "",
+        "",
+        "",
+        "",
     ],
     small_answer_image: [
         "",
-        "https://a.storyblok.com/f/112136/446x446/3ea9d4491c/2.png",
-        "https://a.storyblok.com/f/112136/446x446/1cadc0e6c3/3.png",
-        "https://a.storyblok.com/f/112136/446x446/0c7f465cc6/4.png",
+        "",
+        "",
+        "",
     ],
     background_colour: "rgba(37, 81, 123, 1)",
     background_image: "https://a.storyblok.com/f/112136/1920x1409/5ba98e7f92/texture-bg-5efdcf3715f790-74747584-606d864d1b22d1-55861802.jpg",
